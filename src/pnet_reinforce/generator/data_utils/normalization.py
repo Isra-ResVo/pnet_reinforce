fb5b138c2dba6cd02232fe5b49d07c8b8ea995e8
Isra-ResVo <israelrescalvo@outlook.com>
2023-01-09 15:05:23 -0600

    creation of data utils to manage data basic tranforamations

---
+++ src/pnet_reinforce/generator/data_utils/normalization.py
@@ -0,0 +1,27 @@
+import torch
+
+def normalization(batch: torch.Tensor, device: str = "cpu") -> torch.Tensor:
+    r"""
+    Normalize function -> (0,1)
+    using the next formula by element and caracteristic
+        element - minimum
+        -----------------
+        maximum - minimum
+
+    this works with tensor arrange of the next way
+        [batch index, caracteristic, elements]
+
+
+    Probar: el normalizado entre un rango fijo de valores donde no se tome el maximo y el minomo de cada
+    instancia para tenerlos como litmires naturales del problema
+
+    """
+
+    batchNorm = torch.zeros_like(batch)  # default device of input
+    for i, element in enumerate(batch):
+        for j, parameter in enumerate(element):
+            mini = min(parameter)
+            maxi = max(parameter)
+            batchNorm[i][j] = (parameter - mini) / (maxi - mini)
+
+    return batchNorm
